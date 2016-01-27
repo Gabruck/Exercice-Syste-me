0f8a41ad646d0c610e34c75508a45c695ad5f88a
Exegese <gabriel.collet@y-nov.com>
2016-01-27 15:02:30 +0100

    Exercice 2 - Jour 1

---
+++ Jour 1/Exercice 2/Ex_2.sh
@@ -0,0 +1,18 @@
+mkdir test0 
+chmod 715 test0 
+mkdir test2 
+chmod 504 test2
+
+touch test1
+chmod 414 test1
+touch test3
+chmod 404 test3
+touch test4 
+chmod 020 test4
+touch test5
+chmod 404 test5
+ln -s testJour1 test6
+touch test6
+chmod 777 test6
+
+ls -la
